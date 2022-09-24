42a6983be55bab0278e931ec40e53958c5adaf6c
Hüseyin idin <huseyinidinn@gmail.com>
2022-09-24 12:01:36 +0300

    Perfect Number

---
+++ perfectNumber/src/perfectNumber/Main.java
@@ -0,0 +1,20 @@
+package perfectNumber;
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+		int number = 6;
+		int sum = 0;
+		for (int i = 1; i < number; i++) {
+			if (number % i == 0) {
+				sum += i;
+			}
+		}
+		if (sum == number) {
+			System.out.println("Perfect number: " + sum);
+		} else
+			System.out.println("It's not a perfect number: " + sum);
+	}
+
+}
