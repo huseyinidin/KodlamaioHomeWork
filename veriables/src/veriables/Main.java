02f11bdbfda8345d1733fd22aae6f6820d2b3424
Hüseyin idin <huseyinidinn@gmail.com>
2022-09-24 11:46:41 +0300

    Veriables

---
+++ veriables/src/veriables/Main.java
@@ -0,0 +1,14 @@
+package veriables;
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+		int numberOfStudents = 12;
+
+		String message = "Number of Students : ";
+
+		System.out.println(message + numberOfStudents);
+	}
+
+}
