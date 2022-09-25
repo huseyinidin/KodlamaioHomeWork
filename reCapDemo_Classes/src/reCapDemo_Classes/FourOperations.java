b3e9972b03081c665bd6d8637c41569ba3f2ad2f
Hüseyin idin <huseyinidinn@gmail.com>
2022-09-26 00:59:51 +0300

    Four Operations

---
+++ reCapDemo_Classes/src/reCapDemo_Classes/FourOperations.java
@@ -0,0 +1,21 @@
+package reCapDemo_Classes;
+
+public class FourOperations {
+
+	public int sum(int number1, int number2) {
+		return number1 + number2;
+	}
+
+	public int minus(int number1, int number2) {
+		return number1 - number2;
+	}
+
+	public int multiplication(int number1, int number2) {
+		return number1 * number2;
+	}
+
+	public int devision(int number1, int number2) {
+		return number1 / number2;
+	}
+
+}
