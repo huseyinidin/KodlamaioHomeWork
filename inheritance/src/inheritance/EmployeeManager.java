9e59bb66a527d66eca0327091bcee0cc76a8f2ae
Hüseyin idin <huseyinidinn@gmail.com>
2022-09-27 00:02:41 +0300

    Inheritance

---
+++ inheritance/src/inheritance/EmployeeManager.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class EmployeeManager extends PersonManager{
+	
+	public void bestEmployee() {
+		System.out.println("Employee of the month is listed.");
+	}
+
+}
---
+++ inheritance/src/inheritance/PersonManager.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class PersonManager {
+	
+	public void list() {
+		System.out.println("Listed.");
+	}
+	
+	public void add() {
+		System.out.println("Added.");
+	}
+
+}
