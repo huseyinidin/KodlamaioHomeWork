a75b4510f85f8a38f4beaf9bbaf4e6cc9b3c082f
Hüseyin idin <huseyinidinn@gmail.com>
2022-09-26 23:30:21 +0300

    Constructor

---
+++ classesWithAttributes2/src/classesWithAttributes/Product.java
@@ -0,0 +1,84 @@
+package classesWithAttributes;
+
+public class Product {
+
+	// Parameterless empty constructor method.
+	// public Product() {
+	// System.out.println("The constructor method worked.");
+	// }
+
+	// Parameterized constructor method
+	// Overloading
+	public Product(int id, String name, String description, double price, int stockAmount, String color) {
+		System.out.println("The constructor method worked.");
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.stockAmount = stockAmount;
+	}
+
+	// Attribute | Field
+	private int id;
+	private String name;
+	private String description;
+	private double price;
+	private int stockAmount;
+	private String color;
+	private String code;
+
+	// Getter
+	public int getId() {
+		return id;
+	}
+
+	// Setter
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getStockAmount() {
+		return stockAmount;
+	}
+
+	public void setStockAmount(int stockAmount) {
+		this.stockAmount = stockAmount;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getCode() {
+		return this.name.substring(0, 1) + id;
+	}
+
+}
