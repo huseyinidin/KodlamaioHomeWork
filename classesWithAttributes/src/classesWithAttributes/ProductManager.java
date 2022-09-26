7dba850607fc9a63c1a144bc1ed007d00928c8e6
Hüseyin idin <huseyinidinn@gmail.com>
2022-09-26 22:52:10 +0300

    Encapsulation Getter and Setter

--- classesWithAttributes/src/classesWithAttributes/ProductManager.java
+++ classesWithAttributes/src/classesWithAttributes/ProductManager.java
@@ -3,7 +3,16 @@ package classesWithAttributes;
 public class ProductManager {
 	public void add(Product product) {
 
-		System.out.println("Product added: " + product.name);
+		System.out.println("Product added: " + product.getName());
 	}
+	
+	/* The single responsibility doesn't match the principle.
+	
+	public void add2(int id, String name, String description, double price, int stockAmount, String color) {
+		
+	}
+	*/
+	
+	
 
 }
