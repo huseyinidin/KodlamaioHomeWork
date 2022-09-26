92fd3b49b581eaeef6e508dea1c85e388e516742
Hüseyin idin <huseyinidinn@gmail.com>
2022-09-26 10:22:16 +0300

    Field and Attributes

---
+++ classesWithAttributes/src/classesWithAttributes/Main.java
@@ -0,0 +1,19 @@
+package classesWithAttributes;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Product product = new Product();
+		
+		product.id = 1;
+		product.name ="Asus Laptop";
+		product.price = 5000;
+		product.stockAmount = 3;
+		
+		ProductManager productManager = new ProductManager();
+		productManager.add(product);
+
+	}
+
+}
---
+++ classesWithAttributes/src/classesWithAttributes/Product.java
@@ -0,0 +1,10 @@
+package classesWithAttributes;
+
+public class Product {
+
+	int id;
+	String name;
+	String description;
+	double price;
+	int stockAmount;
+}
