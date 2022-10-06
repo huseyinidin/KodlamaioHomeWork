bc556f76d20d534c963e5d29e19ab4212a7b5719
Hüseyin idin <huseyinidinn@gmail.com>
2022-10-07 00:04:50 +0300

    Class - Method - Inheritance - Stack - Heap

---
+++ week2/classes/src/classes/CustomerManager.java
@@ -0,0 +1,17 @@
+package classes;
+
+public class CustomerManager {
+	
+	public void add() {
+		System.out.println("Customer added. ");
+	}
+	
+	public void remove() {
+		System.out.println("Customer removed. ");
+	}
+	
+	public void update() {
+		System.out.println("Customer updated. ");
+	}
+
+}
