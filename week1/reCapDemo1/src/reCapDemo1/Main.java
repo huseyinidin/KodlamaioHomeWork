6a3a66d21980b5361e9c86629aaa1cfc0c756ef1
Hüseyin idin <huseyinidinn@gmail.com>
2022-10-06 23:42:07 +0300

    ReCap Demo

---
+++ week1/reCapDemo1/src/reCapDemo1/Main.java
@@ -0,0 +1,22 @@
+package reCapDemo1;
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+		int number1 = 20;
+		int number2 = 25;
+		int number3 = 15;
+		int largest = number1;
+		
+		if (largest<number2) {
+			largest = number2;
+		}
+		
+		if (largest<number3) {
+			largest = number3;
+		}
+		System.out.println("Largest : " + largest);
+	}
+
+}
