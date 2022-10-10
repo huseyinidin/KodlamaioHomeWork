dc950bfdae4ddc5f10c58e9657f2576528b363f3
Hüseyin idin <huseyinidinn@gmail.com>
2022-10-11 00:14:32 +0300

    Stack - Heap - Reference Type

---
+++ week3/youTubeEducation/src/Main.java
@@ -0,0 +1,28 @@
+package youTubeEducation.src;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int[] numbers1 = { 1, 2, 3 };
+		int[] numbers2 = { 10, 20, 30 };
+		
+		numbers1 = numbers2;
+		
+		numbers2[0] = 1000;
+		
+		System.out.println(numbers1[0]);
+
+		/*
+		 * int number1 = 10; int number2 = 20;
+		 * 
+		 * number1 = number2; 
+		 * 
+		 * number2 = 100;
+		 * 
+		 * System.out.println(number1);
+		 */
+
+	}
+
+}
