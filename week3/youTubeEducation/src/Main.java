50011a6b68d74132a48129e59f9e94728eb8d9a1
Hüseyin idin <huseyinidinn@gmail.com>
2022-10-12 01:03:33 +0300

    Stack - Heap - Value and Reference Type

--- week3/youTubeEducation/src/Main.java
+++ week3/youTubeEducation/src/Main.java
@@ -5,25 +5,26 @@ public class Main {
 	public static void main(String[] args) {
 
 		// ARRAY REFERENCE TYPE
-				int[] numbers1 = { 1, 2, 3 };
-				int[] numbers2 = { 10, 20, 30 };
-				
-				numbers1 = numbers2;
-				
-				numbers2[0] = 1000;
-				
-				System.out.println(numbers1[0]);
+		int[] numbers1 = { 1, 2, 3 };
+		int[] numbers2 = { 10, 20, 30 };
 
-				/* VALUE TYPE 
-				 * 
-				 * int number1 = 10; int number2 = 20;
-				 * 
-				 * number1 = number2; 
-				 * 
-				 * number2 = 100;
-				 * 
-				 * System.out.println(number1);
-				 */
+		numbers1 = numbers2;
+
+		numbers2[0] = 1000;
+
+		System.out.println(numbers1[0]);
+
+		/*
+		 * VALUE TYPE
+		 * 
+		 * int number1 = 10; int number2 = 20;
+		 * 
+		 * number1 = number2;
+		 * 
+		 * number2 = 100;
+		 * 
+		 * System.out.println(number1);
+		 */
 	}
 
 }
