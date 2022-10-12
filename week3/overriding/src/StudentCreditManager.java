6529374a269245f9eb659d18f1aaecced4f02891
Hüseyin idin <huseyinidinn@gmail.com>
2022-10-12 18:25:08 +0300

    Overriding - Final keyword

---
+++ week3/overriding/src/StudentCreditManager.java
@@ -0,0 +1,10 @@
+package overriding.src;
+
+public class StudentCreditManager extends BaseCreditManager{
+
+	// BaseCreditManager used final keyword can't be overriding.
+	//Overriding
+	/*public double calculate(double price) {
+		return price * 1.10;
+	}*/
+}
