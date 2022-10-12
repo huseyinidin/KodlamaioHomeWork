6ef83c0a3d4158c7977065862fc3fabdbabbc3e0
Hüseyin idin <huseyinidinn@gmail.com>
2022-10-12 22:54:32 +0300

    inner class

---
+++ week3/staticDemo/src/DatabaseHelper.java
@@ -0,0 +1,21 @@
+package staticDemo.src;
+
+// inner class
+public class DatabaseHelper {
+	public static class crud {
+
+		public static void delete() {
+
+		}
+
+		public static void update() {
+
+		}
+	}
+
+	public static class connection {
+
+		public static void createConnection() {
+		}
+	}
+}
