2aa46d45bd143a3c568daa97486d093d362d8dd8
Hüseyin idin <huseyinidinn@gmail.com>
2022-11-26 00:19:54 +0300

    Week 5 - Database Associate

---
+++ week5/kodlamaio/Devs/src/test/java/kodlamaio/Devs/DevsApplicationTests.java
@@ -0,0 +1,13 @@
+package kodlamaio.Devs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
